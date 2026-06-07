3672ba222261204d842087442de24bb28eec680e
respawnloot266-sys <respawnloot266@gmail.com>
2026-06-08 03:45:40 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Telegram Bot Token (BotFather က ပေးတဲ့ token ဒီမှာ ထည့်ပါ)
-BOT_TOKEN =8786662044:AAEyAqYz8MFHM-1e3-TsbwymWB-MgGBjNgM
+BOT_TOKEN ="8614528420:AAFyAx7nPpZPLFSW3jwJ6xzsPvFPyagLSj8"
 
 # OpenChargeMap API Key (နောက်မှ ဖြည့်ပါ)
 OCM_API_KEY = "နောက်မှ_ဖြည့်ပါ"
