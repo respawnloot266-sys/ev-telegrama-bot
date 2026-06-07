753839643def2e7e51fa949b0ab7edd3ecb5f807
respawnloot266-sys <respawnloot266@gmail.com>
2026-06-08 03:11:36 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Telegram Bot Token (BotFather က ပေးတဲ့ token ဒီမှာ ထည့်ပါ)
+BOT_TOKEN =8786662044:AAEyAqYz8MFHM-1e3-TsbwymWB-MgGBjNgM
+
+# OpenChargeMap API Key (နောက်မှ ဖြည့်ပါ)
+OCM_API_KEY = "နောက်မှ_ဖြည့်ပါ"
+
+# Charge station search radius (km)
+SEARCH_RADIUS_KM = 10
+
+# Charge cost per kWh (Ks)
+COST_PER_KWH = 350
+
+# Low battery alert threshold (%)
+LOW_BATTERY_ALERT = 20
