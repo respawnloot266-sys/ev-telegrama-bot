9a8903d11d1fee75fbeb684b1fb1474e1eb03f62
respawnloot266-sys <respawnloot266@gmail.com>
2026-06-08 10:30:05 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove,  KeyboardButtom
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
 
 from config import TELEGRAM_BOT_TOKEN, DEFAULT_LOW_BATTERY_THRESHOLD, CHARGER_TYPES
